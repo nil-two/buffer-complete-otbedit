b4f34bca32c40e62138784c5feb3eed8a9a8fa4d
wara <kusabashira227@gmail.com>
2014-12-13 13:32:01 +0900

    Added source

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
 buffer-complete-otbedit
 =======================
+buffer-complete-otbedit is a macro for [OTBEdit](http://www.hi-ho.ne.jp/a_ogawa/otbedit/).
+This macro provides completion of using buffer.
+Usage
+====================
+1. current buffer is next
+```c
+#include <stdio.h>
 
-バッファ補完を提供します
+int main() {
+	printf("hello\n");
+	|
+}
+```
+(Cursor is in |)
+
+2. hit Ctrl+Space
+
+3. buffer will will next
+```c
+#include <stdio.h>
+
+int main() {
+	printf("hello\n");
+	printf|
+}
+```
+(Cursor is in |)
+
+Installation
+====================
+1. download autoclose-otbedit.zip from the [releases page](https://github.com/kusabashira/buffer-complete-otbedit/releases)
+
+2. Unpack the zip file, and put all in your OTBEdit directory.
+
+3. Write in otbedit.scm in scmlib directory.
+
+```scm
+(use buffer-complete)
+```
+(Please create if otbedit.scm doesn't exist in scmlib directory)
+
+License
+====================
+MIT License
+
+Author
+====================
+wara <kusabashira227@gmail.com>
---
+++ scmlib/buffer-complete.scm
@@ -0,0 +1,84 @@
+(define (load-wara-buffer-complete)
+  (define (current-line)
+    (editor-get-row-string (editor-get-cur-row)))
+
+  (define (current-line-until-current-col)
+    (substring (current-line) 0 (editor-get-cur-col)))
+
+  (define (last-word str)
+    (let ((m (rxmatch-substring (rxmatch #/\w+$/ str))))
+      (if (string? m) m "")))
+
+  (define (current-word)
+    (last-word (current-line-until-current-col)))
+
+  (define (delete-current-word)
+    (editor-backward-char (string-length (current-word)) #t)
+    (editor-delete-selected-string))
+
+  (define (get-all-words str)
+    (let ((hash (make-hash-table 'string=?))
+          (words '()))
+      (regexp-replace-all
+        #/\w+/
+        str
+        (lambda (m)
+          (hash-table-put! hash (rxmatch-substring m) #t)))
+      (hash-table-for-each
+        hash
+        (lambda (k v)
+          (set! words (cons k words))))
+      words))
+
+  (define (select-if proc ls)
+    (let loop ((old-ls ls)
+               (new-ls '()))
+      (if (null? old-ls)
+        (reverse new-ls)
+        (loop (cdr old-ls)
+              (if (proc (car old-ls))
+                (cons (car old-ls) new-ls)
+                new-ls)))))
+
+  (define (has-prefix? str prefix)
+    (let ((str-length (string-length str))
+          (pre-length (string-length prefix)))
+      (and
+        (< pre-length str-length)
+        (string=? prefix (substring str 0 pre-length)))))
+
+  (define (get-possible-completion-list word)
+    (select-if (lambda (s) (has-prefix? s word))
+               (get-all-words (editor-get-all-string))))
+
+  (define complete-now #f)
+  (define complete-list '())
+  (define target-word "")
+
+  (define (start-watching-complete-stop)
+    (app-set-event-handler
+      "on-cursor-moved"
+      (lambda ()
+        (set! complete-now #f)
+        (app-clear-event-handler "on-cursor-moved"))))
+
+  (app-set-key
+    "Ctrl+SPACE"
+    (lambda ()
+      (app-status-bar-msg "")
+      (cond
+        ((not complete-now)
+         (set! target-word (current-word))
+         (set! complete-list (get-possible-completion-list (current-word)))))
+      (delete-current-word)
+      (start-watching-complete-stop)
+      (cond
+        ((null? complete-list)
+         (editor-paste-string target-word)
+         (set! complete-now #f))
+        (else
+          (editor-paste-string (car complete-list))
+          (set! complete-list (cdr complete-list))
+          (set! complete-now #t))))))
+
+(load-wara-buffer-complete)
